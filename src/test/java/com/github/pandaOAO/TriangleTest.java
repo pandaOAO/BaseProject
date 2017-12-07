bd002b2d21569de58c40e7fce9f9f3e28f8a89a0
pandaOAO <pandaOAO@gmail.com>
2017-12-07 20:05:03 +0800

    還沒打好

--- src/test/java/com/github/pandaOAO/TriangleTest.java
+++ src/test/java/com/github/pandaOAO/TriangleTest.java
@@ -1,5 +1,6 @@
 package com.github.pandaOAO;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,12 +8,15 @@ import static org.junit.Assert.*;
 public class TriangleTest {
     @Test
     public void getType() throws Exception {
-        #
-        Triangle triangle = new Triangle(5,10,51);
 
-        #
+        // Arrange
+        Triangle triangle = new Triangle(5, 10, 5);
+
+        // Act
         String result = triangle.getType();
 
+        // Assert
+        Assert.assertEquals("不是三角形", result);
 
     }
 
