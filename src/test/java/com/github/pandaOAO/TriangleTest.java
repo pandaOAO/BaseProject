22ce5a661b364c4743b0bc03258f2a9ca71b9b94
pandaOAO <pandaOAO@gmail.com>
2017-12-07 19:55:20 +0800

    還沒打好

---
+++ src/test/java/com/github/pandaOAO/TriangleTest.java
@@ -0,0 +1,19 @@
+package com.github.pandaOAO;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+    @Test
+    public void getType() throws Exception {
+        #
+        Triangle triangle = new Triangle(5,10,51);
+
+        #
+        String result = triangle.getType();
+
+
+    }
+
+}
