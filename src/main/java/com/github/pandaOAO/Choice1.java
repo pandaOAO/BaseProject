8a8627304b619ffbcf633e929d82cef7f5d1492c
fcu-d0543194 <d0543194@fcu.edu.twd0543194@fcu.edu.twd0543194@fcu.edu.tw>
2018-01-19 12:58:48 +0800

    更新主畫面

---
+++ src/main/java/com/github/pandaOAO/Choice1.java
@@ -0,0 +1,21 @@
+package com.github.pandaOAO;
+
+import java.util.Scanner;
+
+public class Choice1 {
+
+  public Choice1() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public static void choose1() {
+    Scanner scannr = new Scanner(System.in);
+    System.out.print("今天是否有洗頭？1.有2.沒有");
+    boolean IsWashed;
+    int choose;
+    switch (choose) {
+
+    }
+  }
+
+}
--- src/main/java/com/github/pandaOAO/Main.java
+++ src/main/java/com/github/pandaOAO/Main.java
@@ -1,7 +1,48 @@
 package com.github.pandaOAO;
 
+import java.util.Scanner;
+
 public class Main {
-  public int add(int a, int b) {
-    return a - b;
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+    chooseList();
+
   }
+
+  public static void chooseList() {
+
+    System.out.println("你有什麼樣的困擾呢？");
+    System.out.println("1.懶得洗頭");
+    System.out.println("2.眼睛太大");
+    System.out.println("3.被自己帥醒");
+    System.out.println("4.覺得自己太美");
+    System.out.println("5.一直想上廁所");
+    System.out.print("請選擇：");
+
+    int choose;
+
+    Scanner scanner = new Scanner(System.in);
+    choose = scanner.nextInt();
+
+    switch (choose) {
+
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    default:
+      chooseList();
+      break;
+    }
+    scanner.close();
+  }
+
 }
